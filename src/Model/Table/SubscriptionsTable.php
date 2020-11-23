8f9809ad9147d039249dd3f0b9e82dc13f060204
Hendrik Schmeer <mail@hendrikschmeer.de>
2020-11-23 20:20:32 +0100

    subscription and notification models

---
+++ src/Model/Table/SubscriptionsTable.php
@@ -0,0 +1,101 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Subscriptions Model
+ *
+ * @property \App\Model\Table\NotificationsTable&\Cake\ORM\Association\HasMany $Notifications
+ *
+ * @method \App\Model\Entity\Subscription get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Subscription newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Subscription[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Subscription|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Subscription saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Subscription patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Subscription[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Subscription findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class SubscriptionsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('subscriptions');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->hasMany('Notifications', [
+            'foreignKey' => 'subscription_id',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->email('email')
+            ->requirePresence('email', 'create')
+            ->notEmptyString('email')
+            ->add('email', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->boolean('online_course')
+            ->allowEmptyString('online_course');
+
+        $validator
+            ->boolean('confirmed')
+            ->notEmptyString('confirmed');
+
+        $validator
+            ->scalar('confirmation_key')
+            ->maxLength('confirmation_key', 255)
+            ->requirePresence('confirmation_key', 'create')
+            ->notEmptyString('confirmation_key');
+
+        $validator
+            ->scalar('deletion_key')
+            ->maxLength('deletion_key', 255)
+            ->allowEmptyString('deletion_key');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['email']));
+
+        return $rules;
+    }
+}
---
+++ tests/TestCase/Model/Table/SubscriptionsTableTest.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\SubscriptionsTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\SubscriptionsTable Test Case
+ */
+class SubscriptionsTableTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\SubscriptionsTable
+     */
+    public $Subscriptions;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.Subscriptions',
+        'app.Notifications',
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::getTableLocator()->exists('Subscriptions') ? [] : ['className' => SubscriptionsTable::class];
+        $this->Subscriptions = TableRegistry::getTableLocator()->get('Subscriptions', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Subscriptions);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test validationDefault method
+     *
+     * @return void
+     */
+    public function testValidationDefault()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test buildRules method
+     *
+     * @return void
+     */
+    public function testBuildRules()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
