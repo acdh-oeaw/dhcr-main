95a2b760db3676c1d9eabd98f0e92967ed89fec6
Hendrik Schmeer <mail@hendrikschmeer.de>
2021-05-05 00:24:32 +0200

    Adding User classes to app

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,141 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Event\Event;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ *
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+
+
+
+
+    public function beforeFilter(Event $event) {
+        parent::beforeFilter($event);
+
+        if($this->Auth->user('user_role_id') < 3) $this->Auth->allow(array('invite'));
+
+        if($this->Auth->user()) {
+            if($this->DefaultAuth->isAdmin()) {
+                $this->Auth->allow(array('delete'));
+            }
+            $this->Auth->allow(array('delete_identity'));
+        }
+
+        $this->set('title_for_layout', 'Account Management');
+    }
+
+
+
+
+
+
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['UserRoles', 'Countries', 'Institutions'],
+        ];
+        $users = $this->paginate($this->Users);
+
+        $this->set(compact('users'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => ['UserRoles', 'Countries', 'Institutions', 'Courses'],
+        ]);
+
+        $this->set('user', $user);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $userRoles = $this->Users->UserRoles->find('list', ['limit' => 200]);
+        $countries = $this->Users->Countries->find('list', ['limit' => 200]);
+        $institutions = $this->Users->Institutions->find('list', ['limit' => 200]);
+        $this->set(compact('user', 'userRoles', 'countries', 'institutions'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $userRoles = $this->Users->UserRoles->find('list', ['limit' => 200]);
+        $countries = $this->Users->Countries->find('list', ['limit' => 200]);
+        $institutions = $this->Users->Institutions->find('list', ['limit' => 200]);
+        $this->set(compact('user', 'userRoles', 'countries', 'institutions'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->Users->get($id);
+        if ($this->Users->delete($user)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ src/Model/Table/UsersTable.php
@@ -0,0 +1,194 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ *
+ * @property \App\Model\Table\UserRolesTable&\Cake\ORM\Association\BelongsTo $UserRoles
+ * @property \App\Model\Table\CountriesTable&\Cake\ORM\Association\BelongsTo $Countries
+ * @property \App\Model\Table\InstitutionsTable&\Cake\ORM\Association\BelongsTo $Institutions
+ * @property \App\Model\Table\CoursesTable&\Cake\ORM\Association\HasMany $Courses
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class UsersTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('users');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('UserRoles', [
+            'foreignKey' => 'user_role_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Countries', [
+            'foreignKey' => 'country_id',
+        ]);
+        $this->belongsTo('Institutions', [
+            'foreignKey' => 'institution_id',
+        ]);
+        $this->hasMany('Courses', [
+            'foreignKey' => 'user_id',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('university')
+            ->maxLength('university', 255)
+            ->allowEmptyString('university');
+
+        $validator
+            ->email('email')
+            ->requirePresence('email', 'create')
+            ->notEmptyString('email')
+            ->add('email', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->scalar('shib_eppn')
+            ->maxLength('shib_eppn', 255)
+            ->allowEmptyString('shib_eppn')
+            ->add('shib_eppn', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
+
+        $validator
+            ->scalar('password')
+            ->maxLength('password', 255)
+            ->allowEmptyString('password');
+
+        $validator
+            ->boolean('email_verified')
+            ->notEmptyString('email_verified');
+
+        $validator
+            ->boolean('active')
+            ->notEmptyString('active');
+
+        $validator
+            ->boolean('approved')
+            ->notEmptyString('approved');
+
+        $validator
+            ->boolean('is_admin')
+            ->notEmptyString('is_admin');
+
+        $validator
+            ->boolean('user_admin')
+            ->notEmptyString('user_admin');
+
+        $validator
+            ->dateTime('last_login')
+            ->allowEmptyDateTime('last_login');
+
+        $validator
+            ->scalar('password_token')
+            ->maxLength('password_token', 255)
+            ->allowEmptyString('password_token');
+
+        $validator
+            ->scalar('email_token')
+            ->maxLength('email_token', 255)
+            ->allowEmptyString('email_token');
+
+        $validator
+            ->scalar('approval_token')
+            ->maxLength('approval_token', 255)
+            ->allowEmptyString('approval_token');
+
+        $validator
+            ->scalar('new_email')
+            ->maxLength('new_email', 255)
+            ->allowEmptyString('new_email');
+
+        $validator
+            ->dateTime('password_token_expires')
+            ->allowEmptyDateTime('password_token_expires');
+
+        $validator
+            ->dateTime('email_token_expires')
+            ->allowEmptyDateTime('email_token_expires');
+
+        $validator
+            ->dateTime('approval_token_expires')
+            ->allowEmptyDateTime('approval_token_expires');
+
+        $validator
+            ->scalar('last_name')
+            ->maxLength('last_name', 255)
+            ->allowEmptyString('last_name');
+
+        $validator
+            ->scalar('first_name')
+            ->maxLength('first_name', 255)
+            ->allowEmptyString('first_name');
+
+        $validator
+            ->scalar('academic_title')
+            ->maxLength('academic_title', 255)
+            ->allowEmptyString('academic_title');
+
+        $validator
+            ->scalar('about')
+            ->allowEmptyString('about');
+
+        $validator
+            ->boolean('mail_list')
+            ->notEmptyString('mail_list');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['email']));
+        $rules->add($rules->isUnique(['shib_eppn']));
+        $rules->add($rules->existsIn(['user_role_id'], 'UserRoles'));
+        $rules->add($rules->existsIn(['country_id'], 'Countries'));
+        $rules->add($rules->existsIn(['institution_id'], 'Institutions'));
+
+        return $rules;
+    }
+}
