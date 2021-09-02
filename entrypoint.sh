14a284e13e377a8a732d6390997d9002962f446d
Dalibor Pančić <dalibor.pancic@gmail.com>
2021-09-02 16:22:02 +0200

    downgrade php to 7.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+su herokuishuser
+cp /app/composer.phar /app/ops/app/
+cp /app/composer.phar /app/api/v1/
+cd /app/ops/app/ && /app/.heroku/php/bin/php composer.phar update
+cd /app/api/v1/ && /app/.heroku/php/bin/php composer.phar update
+
 # Run cake discovery before starting web server
 bin/cake discovery
 # Start web server
