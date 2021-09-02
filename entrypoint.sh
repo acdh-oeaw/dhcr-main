357526ce5f6c57e1255674d50246e32ab644082a
Dalibor Pančić <dalibor.pancic@gmail.com>
2021-09-02 10:39:00 +0200

    try heroku-buildpack-apt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cp dhcr-cron /etc/cron.d/ 
+chmod 0644 /etc/cron.d/dhcr-cron
+crontab /etc/cron.d/dhcr-cron 
 cron -f &
-/var/www/html/bin/cake discovery
-docker-php-entrypoint apache2-foreground
+
+bin/cake discovery
+
+exec heroku-php-apache2 
