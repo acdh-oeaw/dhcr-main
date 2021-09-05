2ccfc904667066571da3421dfd48c289766ccc86
Dalibor Pančić <dalibor.pancic@gmail.com>
2021-09-05 15:15:07 +0200

    hardcode env variable

--- config/app.php
+++ config/app.php
@@ -276,7 +276,7 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => env('DB_HOST', null),
+            'host' => env('DB_HOST', 'helios.arz.oeaw.ac.at'),
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
