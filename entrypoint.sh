63709b588090fd9d55f95f3238a64070acc298dd
Dalibor Pančić <dalibor.pancic@gmail.com>
2021-09-05 14:26:02 +0200

    try to move commands to composer

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@
 su herokuishuser
 export PATH="/app/.heroku/php/bin:${PATH}"
 
-cp /app/composer.phar /app/ops/app/
-cp /app/composer.phar /app/api/v1/
-cd /app/ops/app/ && php composer.phar update
-cd /app/api/v1/ && php composer.phar update
-cd /app/
-
 bin/cake discovery
 bin/cake plugin load DebugKit
 
