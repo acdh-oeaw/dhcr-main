0f1ea3b531c23c2e09c74458a59255512282f17c
Dalibor Pančić <dalibor.pancic@gmail.com>
2021-09-05 13:26:23 +0200

    try without test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ cd /app/ops/app/ && /app/.heroku/php/bin/php composer.phar update
 cd /app/api/v1/ && /app/.heroku/php/bin/php composer.phar update
 cd /app/
 
+bin/cake plugin load DebugKit
+bin/cake discovery
+
 # Start web server
 exec heroku-php-apache2 
