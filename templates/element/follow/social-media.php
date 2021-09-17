c8d95c2e7855cbb92676683cf58a0afadc19c740
Hendrik Schmeer <mail@hendrikschmeer.de>
2021-09-17 11:21:50 +0200

    social media update

--- templates/element/follow/social-media.php
+++ templates/element/follow/social-media.php
@@ -1,10 +1,27 @@
 <?php $this->Html->css('https://use.fontawesome.com/releases/v5.8.2/css/all.css', ['block' => true]); ?>
 
-
 <h3 class="follow">
-    <a href="https://instagram.com/dhcourseregistry">
+    <a href="https://instagram.com/dhcourseregistry" target="_blank">
        <span class="fab fa-instagram"></span>
-       Follow us on Instagram!
+       Instagram
+    </a>
+</h3>
+<h3 class="follow">
+    <a href="https://twitter.com/hashtag/dhcourseregistry?src=hash" target="_blank">
+        <span class="fab fa-twitter"></span>
+        Twitter
+    </a>
+</h3>
+<h3 class="follow">
+    <a href="https://www.facebook.com/hashtag/dhcourseregistry" target="_blank">
+        <span class="fab fa-facebook"></span>
+        Facebook
+    </a>
+</h3>
+<h3 class="follow">
+    <a href="https://www.youtube.com/watch?v=D-5kmQoPvOU&list=PLfWGHIkSIx0V2mYqdbfRHOHuBz-CbX4EW" target="_blank">
+        <span class="fab fa-youtube"></span>
+        YouTube
     </a>
 </h3>
 
