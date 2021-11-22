d269b05c65a6e72c710ea9ca2ba8b05c40cd7957
patrickakk <patrick@algority.eu>
2021-11-22 10:08:09 +0100

    created pagination templates

---
+++ config/paginator-templates.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'number' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;&nbsp;',
+    'first' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;&nbsp;',
+    'current' => '<em><u><a href="{{url}}">{{text}}</a></u></em>&nbsp;&nbsp;&nbsp;&nbsp;',
+    'prevActive' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;',
+    'prevDisabled' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;',
+    'nextActive' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;',
+    'nextDisabled' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;',
+    'last' => '<a href="{{url}}">{{text}}</a>&nbsp;&nbsp;&nbsp;',
+];
--- src/View/AppView.php
+++ src/View/AppView.php
@@ -37,5 +37,6 @@ class AppView extends View
     public function initialize() : void
     {
         $this->loadHelper('Authentication.Identity');
+        $this->loadHelper('Paginator', ['templates' => 'paginator-templates']);
     }
 }
