67e187a7f4b74ac71adfbb284b6c3117672bb2c7
patrickakk <patrick@algority.eu>
2022-01-10 14:45:18 +0100

    Revert "Revert "updated Dashboardcontroller course expiry time""
    This reverts commit bcbf4dd08552cd70ce819aa466fc9d062d032123.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -34,8 +34,7 @@ class DashboardController extends AppController
         $pendingAccountRequests = $this->Users->find()->where(['approved' => 0])->count();
         $pendingCourseRequests = $this->Courses->find()->where(['approved' => 0])->count();
 
-        // todo: check exact period for expiry mails
-        $expiryDate = new FrozenDate('-9 months');
+        $expiryDate = new FrozenDate('-10 months'); // in new implementation the expiry mails will be sent after 10 months
         $expiredCourses = $this->Courses->find()
             ->where([
                 'updated <=' => $expiryDate,
