e187d433c8d568a0905e2436c7d9c95047c0de17
patrickakk <patrick@algority.eu>
2022-03-21 15:51:13 +0100

    added -fullname- explanation to user interface

--- templates/InviteTranslations/add.php
+++ templates/InviteTranslations/add.php
@@ -1,9 +1,3 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\InviteTranslation $inviteTranslation
- */
-?>
 <div class="row">
     <p></p>
     <h2><span class="glyphicon glyphicon-text-color"></span>&nbsp;&nbsp;&nbsp;Add Invite Translation</h2>
@@ -13,10 +7,12 @@
             <fieldset>
                 <legend><?= __('Add Invite Translation') ?></legend>
                 <?php
-                    echo $this->Form->control('name');
+                    echo $this->Form->control('name', ['label' => 'Translation Name / Language']);
                     echo $this->Form->control('subject');
-                    echo $this->Form->control('messageBody');
-                    echo $this->Form->control('messageSignature');
+                    echo '<p>Note: The text <strong><i>-fullname-</i></strong> in the message is required and will be 
+                        automatically replaced by the academic title, first name and last name of the person who is sending the invitation. 
+                        Please do not change or translate it. Take a look at the English translation as an example of how to use it.</p>';
+                    echo $this->Form->control('messageBody', ['label' => 'Message']);
                     echo $this->Form->control('active');
                 ?>
             </fieldset>
--- templates/InviteTranslations/edit.php
+++ templates/InviteTranslations/edit.php
@@ -8,10 +8,12 @@
             <fieldset>
                 <legend><?= __('Edit Invite Translation') ?></legend>
                 <?php
-                    echo $this->Form->control('name');
+                    echo $this->Form->control('name', ['label' => 'Translation Name / Language']);
                     echo $this->Form->control('subject');
-                    echo $this->Form->control('messageBody');
-                    echo $this->Form->control('messageSignature');
+                    echo '<p>Note: The text <strong><i>-fullname-</i></strong> in the message is required and will be 
+                        automatically replaced by the academic title, first name and last name of the person who is sending the invitation. 
+                        Please do not change or translate it. Take a look at the English translation as an example of how to use it.</p>';
+                    echo $this->Form->control('messageBody', ['label' => 'Message']);
                     echo $this->Form->control('active');
                 ?>
             </fieldset>
