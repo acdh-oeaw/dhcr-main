92764d5c56fbba32900a18d174f21235795fd7a0
patrickakk <patrick@algority.eu>
2022-03-22 01:01:03 +0100

    updated cities controller

--- src/Controller/CitiesController.php
+++ src/Controller/CitiesController.php
@@ -6,27 +6,14 @@ namespace App\Controller;
 
 use Cake\Event\EventInterface;
 
-/**
- * Cities Controller
- *
- * @method \App\Model\Entity\City[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
 class CitiesController extends AppController
 {
     public function beforeRender(EventInterface $event)
     {
         parent::beforeRender($event);
-        // required for contributors menu
-        $user = $this->Authentication->getIdentity();
-        $this->set('user_role_id', $user->user_role_id);
         $this->viewBuilder()->setLayout('contributors');
     }
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
     public function index()
     {
         // Set breadcrums
@@ -42,14 +29,11 @@ class CitiesController extends AppController
             'contain' => ['Countries'],
             'order' => ['id' => 'asc']
         ]);
+        $user = $this->Authentication->getIdentity();
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('cities'));
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
         // Set breadcrums
@@ -69,23 +53,16 @@ class CitiesController extends AppController
             $city = $this->Cities->patchEntity($city, $this->request->getData());
             if ($this->Cities->save($city)) {
                 $this->Flash->success(__('The city has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The city could not be saved. Please, try again.'));
         }
-
         $countries = $this->Cities->Countries->find('list', ['order' => 'Countries.name asc']);
+        $user = $this->Authentication->getIdentity();
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('city', 'countries'));
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id City id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function edit($id = null)
     {
         // Set breadcrums
@@ -105,33 +82,13 @@ class CitiesController extends AppController
             $city = $this->Cities->patchEntity($city, $this->request->getData());
             if ($this->Cities->save($city)) {
                 $this->Flash->success(__('The city has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The city could not be saved. Please, try again.'));
         }
-
         $countries = $this->Cities->Countries->find('list', ['order' => 'Countries.name asc'])->all();
+        $user = $this->Authentication->getIdentity();
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('city', 'countries'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id City id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $city = $this->Cities->get($id);
-        if ($this->Cities->delete($city)) {
-            $this->Flash->success(__('The city has been deleted.'));
-        } else {
-            $this->Flash->error(__('The city could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
