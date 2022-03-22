f5be7ca7b86978e1372752bdb56d9c1dfbc653d2
patrickakk <patrick@algority.eu>
2022-03-22 02:44:01 +0100

    updated languagescontroller

--- src/Controller/LanguagesController.php
+++ src/Controller/LanguagesController.php
@@ -6,31 +6,18 @@ namespace App\Controller;
 
 use Cake\Event\EventInterface;
 
-/**
- * Languages Controller
- *
- * @property \App\Model\Table\LanguagesTable $Languages
- * @method \App\Model\Entity\Language[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
 class LanguagesController extends AppController
 {
     public function beforeRender(EventInterface $event)
     {
         parent::beforeRender($event);
-        // required for contributors menu
-        $user = $this->Authentication->getIdentity();
-        $this->set('user_role_id', $user->user_role_id);
         $this->viewBuilder()->setLayout('contributors');
     }
 
-
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null|void Renders view
-     */
     public function index()
     {
+        $user = $this->Authentication->getIdentity();
+        // todo add auth
         // Set breadcrums
         $breadcrumTitles[0] = 'Category Lists';
         $breadcrumControllers[0] = 'Dashboard';
@@ -39,19 +26,15 @@ class LanguagesController extends AppController
         $breadcrumControllers[1] = 'Languages';
         $breadcrumActions[1] = 'index';
         $this->set((compact('breadcrumTitles', 'breadcrumControllers', 'breadcrumActions')));
-
         $languages = $this->paginate($this->Languages);
-
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('languages'));
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
+        $user = $this->Authentication->getIdentity();
+        // todo add auth
         // Set breadcrums
         $breadcrumTitles[0] = 'Category Lists';
         $breadcrumControllers[0] = 'Dashboard';
@@ -63,29 +46,23 @@ class LanguagesController extends AppController
         $breadcrumControllers[2] = 'Languages';
         $breadcrumActions[2] = 'add';
         $this->set((compact('breadcrumTitles', 'breadcrumControllers', 'breadcrumActions')));
-
         $language = $this->Languages->newEmptyEntity();
         if ($this->request->is('post')) {
             $language = $this->Languages->patchEntity($language, $this->request->getData());
             if ($this->Languages->save($language)) {
                 $this->Flash->success(__('The language has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The language could not be saved. Please, try again.'));
         }
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('language'));
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Language id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function edit($id = null)
     {
+        $user = $this->Authentication->getIdentity();
+        // todo add auth
         // Set breadcrums
         $breadcrumTitles[0] = 'Category Lists';
         $breadcrumControllers[0] = 'Dashboard';
@@ -97,37 +74,16 @@ class LanguagesController extends AppController
         $breadcrumControllers[2] = 'Languages';
         $breadcrumActions[2] = 'edit';
         $this->set((compact('breadcrumTitles', 'breadcrumControllers', 'breadcrumActions')));
-
         $language = $this->Languages->get($id);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $language = $this->Languages->patchEntity($language, $this->request->getData());
             if ($this->Languages->save($language)) {
                 $this->Flash->success(__('The language has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The language could not be saved. Please, try again.'));
         }
+        $this->set(compact('user')); // required for contributors menu
         $this->set(compact('language'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Language id.
-     * @return \Cake\Http\Response|null|void Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $language = $this->Languages->get($id);
-        if ($this->Languages->delete($language)) {
-            $this->Flash->success(__('The language has been deleted.'));
-        } else {
-            $this->Flash->error(__('The language could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
