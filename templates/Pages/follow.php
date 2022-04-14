a989f579071b89ac16818c8297ef389a7613b077
patrickakk <patrick@algority.eu>
2022-04-14 19:29:31 +0200

    minor change to pages, follow

--- templates/Pages/follow.php
+++ templates/Pages/follow.php
@@ -1,7 +1,7 @@
 <div class="title">
     <h2>Follow the DH Course Registry</h2>
     <p>Subscribe for email alerts on new courses or follow us on social media.<br>
-    Lecturers can sign up for the Contributor Mailing List.</p>
+    As a lecturer, national moderator or programme director can sign up for the Contributor Mailing List.</p>
 </div>
 <div id="accordeon">
     <div class="accordeon-item" id="course-alert">
