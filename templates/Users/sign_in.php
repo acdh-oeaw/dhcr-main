bf38d18f9606aca4f8130c2d575f2498211e98b3
patrickakk <patrick@algority.eu>
2022-05-26 23:57:36 +0200

    updated texts 2

--- templates/Users/sign_in.php
+++ templates/Users/sign_in.php
@@ -6,15 +6,16 @@ $this->set('bodyClasses', 'login');
 ?>
 <div id="classicLogin" style="display:none" class="loginAlternative">
     <h2>Login</h2>
-    <p>Use your e-mail address and DHCR password to log in. <br>
-        Reset your password if you forgot it or never set one.</p>
-    <p>If you do not have a DHCR account<?php // and were not able to log in using the federated login ?>,
-        please use the <?= $this->Html->link('registration form', '/users/register') ?>.</p>
-    <p class="notice">You only need to sign-in to the DHRC, if you are a lecturer or other academic institution member
-        and want to add or curate course meta data.
-        All other audience may use the publicly available content and functionality freely.
-        See this <a href="https://www.youtube.com/embed/s-YsnpKCaUE">tutorial video</a> for advice
-        on how to browse or filter our listed courses.</p>
+    <ul>
+        <li>Use your e-mail address and DHCR password to log in.</li>
+        <li>Reset your password if you forgot it or never set one.</li>
+        <li>If you do not have a DHCR account<?php // and were not able to log in using the federated login ?>, 
+        please use the <?= $this->Html->link('registration form', '/users/register') ?> to create one.</li>
+    </ul>
+    <p class="notice">You need to login to the DHCR if you are a lecturer or other academic institution member and 
+        want to add or curate course metadata. All other audience may use the publicly available content and functionality 
+        freely. Watch this <a href="https://www.youtube.com/embed/s-YsnpKCaUE">video tutorial</a> to learn how to browse or 
+        filter the courses in the registry.</p>
     <div class="users form">
         <?= $this->Form->create() ?>
         <?= $this->Form->control('email') ?>
