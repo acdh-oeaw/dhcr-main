c75f33eea079f548fce68f609e6ef6d9813eab30
patrickakk <patrick@algority.eu>
2022-06-21 19:28:27 +0200

    added content for processes explantion

--- templates/Help/processes_explanation.php
+++ templates/Help/processes_explanation.php
@@ -1,31 +1,115 @@
 <div class="help content">
     <p></p>
     <h2><span class="glyphicon glyphicon-wrench"></span>&nbsp;&nbsp;&nbsp;Processes explanation</h2>
+    <p>Version: 21-06-2022</p>
     <strong><u>Contents</u></strong>
-    <ul>
-        <li><a href="#question1">Question 1 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
-        <li><a href="#question2">Question 2 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
-        <li><a href="#question3">Question 3 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
-    </ul>
+    <ol>
+        <li><a href="#ch1">What is the DHCR?</a></li>
+        <li><a href="#ch2">User roles</a></li>
+        <li><a href="#ch3">Account creation</a></li>
+        <li><a href="#ch4">Course reminder mails / expiry process</a></li>
+        <li><a href="#ch5">Emails sent by the application</a></li>
+    </ol>
     <p>&nbsp;</p>
-    <h3 id="question1">Question 1 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
-    <h4>Answer 1 - Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
-        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor 
-        at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem 
-        Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
-        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" </h4>
-    <p>&nbsp;</p>
-    <h3 id="question2">Question 2 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
-    <h4>Answer 2 - Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
-        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor 
-        at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem 
-        Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
-        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" </h4>
-    <p>&nbsp;</p>
-    <h3 id="question3">Question 3 - Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
-    <h4>Answer 3 - Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
-        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor 
-        at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem 
-        Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
-        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" </h4>
+    <ol>
+        <h3 id="ch1">
+            <li>What is the DHCR?</li>
+        </h3>
+        <h4>
+            <p>The Digital Humanities Course Registry contains an up-to-date overview of courses in the field of digital humanities worldwide.
+                More info can be found at <a href="https://dhcr.clarin-dariah.eu/info">https://dhcr.clarin-dariah.eu/info</a>
+            </p>
+        </h4>
+        <p>&nbsp;</p>
+        <h3 id="ch2">
+            <li>User roles</li>
+        </h3>
+        <u>Public access</u>
+        <h4>
+            <p>The registry is public available to everyone, without login or account.</p>
+
+            <u>Contributors</u>
+            <p>For lecturers it's possible to create an account and add courses to the registry.
+            <ul>
+                <li>After registering for an account, this needs to be approved by the moderator or one of the administrators.
+                    The account can't be used until it is approved.</li>
+                <li>After adding a course, this needs to be approved by the moderator or one of the administrators. The course won't be
+                    shown in the registry until it is approved.</li>
+            </ul>
+            </p>
+
+            <u>Moderators</u>
+            <p>For most countries there is a moderator which curates the country and can approve courses and users in this country.
+                There are several features available to manage the courses and users in the country.</p>
+
+            <u>Administrators</u>
+            <p>Countries without moderator are managed by the administrators.</p>
+        </h4>
+        <p>&nbsp;</p>
+        <h3 id="ch3">
+            <li>Account creation</li>
+        </h3>
+        <h4>
+            <p>There are two ways to create an account:</p>
+            <u>3.1 Registration form</u>
+            <p>Lecturers can register for an account at <a href="https://dhcr.clarin-dariah.eu/users/register">
+                    https://dhcr.clarin-dariah.eu/users/register</a>. After submitting the registration form, the following steps need to be completed:
+            <ul>
+                <li>Confirm email address</li>
+                <li>Set password</li>
+                <li>(Wait for) account approval by moderator</li>
+            </ul>
+            </p>
+            <u>3.2 Invitation by moderator</u>
+            <p>Moderators can invite users through the application. In this case the lecturer receives an email with an invitation.
+                The sender of this mail is the moderator and as well a bcc of the mail is sent to the moderator.<br>
+                Also fewer following steps are required for the lecturer.
+            <ul>
+                <li>As moderator visit <a href="https://dhcr.clarin-dariah.eu/users/invite">https://dhcr.clarin-dariah.eu/users/invite</a>
+                    (maybe you need to login)</li>
+                <li>Fill out the form and choose a localization. The email address of the lecturer is also entered in this step.</li>
+                <li>The lecturer receives an email with a link to set a password. This link is valid for 24 hours.</li>
+                <li>Not needed: confirmation of email address (already provided by moderator)</li>
+                <li>Not needed: account approval (sending the invitation includes "instant approval")</li>
+            </ul>
+            </p>
+        </h4>
+        <p>&nbsp;</p>
+        <h3 id="ch4">
+            <li>Course reminder mails / expiry process</li>
+        </h3>
+        <h4>
+            <p>As a way to keep the course meta data up to date, reminder emails are sent and there is a traffic-light color which
+                indicates when the course was last updated.<br>
+            --To be updated below:-
+            <ul>
+                <li>When a course is entered it is shown in green.</li>
+                <li>After 300 days (ca. 10 months) the course owner receives emails with the request to update the course. The course stays
+                    green in the list.</li>
+                <li>After 331 days (ca 11 months) the course is shown yellow in the list.</li>
+                <li>When 60 days have been gone since the first reminder mail was set to the course owner (The course haven't updated for 360 days),
+                    the moderator will be on cc of the reminder mails.</li>
+                <li>Course is shown red, when ... more to be added</li>
+            </ul>
+            </p>
+        </h4>
+        <p>&nbsp;</p>
+        <h3 id="ch5">
+            <li>Emails sent by the application</li>
+        </h3>
+        <h4>
+            <u>5.1 Invitation</u><br>
+            <p>To be added</p>
+            <u>5.2 Reminder</u><br>
+            <p>To be added</p>
+            <u>5.3 Contact form</u><br>
+            <p>To be added</p>
+            <u>5.4 Subscriptions</u><br>
+            <p>To be added</p>
+            <u>5.5 Contributor mailing list</u><br>
+            <p>To be added</p>
+    </ol>
+    </p>
+    </h4>
+    </ol>
 </div>
