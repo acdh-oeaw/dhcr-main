a03272da4978737f2859c4fbcf5f4c0c42b553f7
patrickakk <patrick@algority.eu>
2022-07-12 22:20:27 +0200

    updated password reset email template

--- templates/email/text/users/reset_password.php
+++ templates/email/text/users/reset_password.php
@@ -5,6 +5,6 @@ use Cake\Core\Configure;
 echo "To change your password, click the link below.\n";
 echo "If you did not request the password reset, you can ignore this email.\n";
 echo "\n";
-echo Configure::read('dhcr.baseUrl') . 'users/reset_password/' . $user->password_token;
+echo Configure::read('dhcr.baseUrl') . 'users/reset_password/' . $user->password_token . "\n";
 echo "\n";
 echo "This link is valid for 1 hour, until " . $user->password_token_expires->i18nFormat('HH:mm:ss') . " UTC.\n";
