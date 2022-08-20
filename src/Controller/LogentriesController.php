604603493de5491d3362e1249ff95624659bf4dc
patrickakk <patrick@algority.eu>
2022-08-20 16:55:56 +0200

    removed log view from controller

--- src/Controller/LogentriesController.php
+++ src/Controller/LogentriesController.php
@@ -37,25 +37,4 @@ class LogentriesController extends AppController
         $this->set(compact('user')); // required for contributors menu
         $this->set(compact('logentries'));
     }
-
-    public function view($id = null)
-    {
-        $logentry = $this->Logentries->get($id, ['contain' => ['LogentryCodes', 'Users']]);
-        $user = $this->Authentication->getIdentity();
-        $this->Authorization->authorize($logentry);
-        // Set breadcrums
-        $breadcrumTitles[0] = 'Category Lists';
-        $breadcrumControllers[0] = 'Dashboard';
-        $breadcrumActions[0] = 'categoryLists';
-        $breadcrumTitles[1] = 'Log Entries';
-        $breadcrumControllers[1] = 'Logentries';
-        $breadcrumActions[1] = 'index';
-        $breadcrumTitles[2] = 'Log Entry Details';
-        $breadcrumControllers[2] = 'Logentries';
-        $breadcrumActions[2] = 'view';
-        $this->set((compact('breadcrumTitles', 'breadcrumControllers', 'breadcrumActions')));
-        $logentriesMax = $this->Logentries->find('all', ['order' => 'id desc'])->first()->id;
-        $this->set(compact('user')); // required for contributors menu
-        $this->set(compact('logentry', 'logentriesMax'));
-    }
 }
