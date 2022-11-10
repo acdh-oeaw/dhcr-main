bd1db262feab206fef8e806f5fc6e9daaaa673f4
patrickakk <patrick@algority.eu>
2022-11-10 17:40:54 +0100

    restructured course statistics view

--- templates/Statistics/course_statistics.php
+++ templates/Statistics/course_statistics.php
@@ -2,40 +2,6 @@
 
 use Cake\I18n\FrozenTime;
 ?>
-<div class="statistics content">
-    <p></p>
-    <h2><span class="glyphicon glyphicon-education"></span>&nbsp;&nbsp;&nbsp;Course Statistics</h2>
-    <?php
-    // guard against providing not accurate data
-    if (env('DHCR_BASE_URL') != 'https://dhcr.clarin-dariah.eu/') {
-        echo '<font color="red"><strong>';
-        echo '**************************************************<br>';
-        echo 'WARNING:<br>';
-        echo 'You are not using the production version.<br>';
-        echo 'This data is not accurate.<br>';
-        echo 'Do not use this data!<br>';
-        echo '**************************************************<br>';
-        echo '</strong></font><br>';
-    }
-    $now = new FrozenTime('now');
-    echo '<p><u>Data updated at: ' . $now->i18nFormat('dd-MM-yyyy HH:mm') . ' UTC</u></p>';
-    ?>
-    <h3>Key data</h3>
-    <p>
-        Total: <?= $coursesTotal ?><br>
-        In backend & published: <?= $coursesBackend ?><br>
-        Public visible: <?= $coursesPublic ?><br>
-        Public as part of backend: <?= (int) ($coursesPublic / $coursesBackend * 100) ?>%
-    </p>
-    <h3>Total updated courses until months ago</h3>
-    <div id="chart_updated_courses"></div>
-    <p></p>
-    <h3>Amount of courses that will be archived soon</h3>
-    <div id="chart_archived_soon_courses"></div>
-    <p></p>
-    <p><i>Note: These statistics can change at any time. For example when a user makes changes to a course or when a certain
-            expiration period has exceeded.</i></p>
-</div>
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script>
@@ -91,3 +57,38 @@ use Cake\I18n\FrozenTime;
         chart2.draw(data2, options2);
     }
 </script>
+
+<div class="statistics content">
+    <p></p>
+    <h2><span class="glyphicon glyphicon-education"></span>&nbsp;&nbsp;&nbsp;Course Statistics</h2>
+    <?php
+    // guard against providing not accurate data
+    if (env('DHCR_BASE_URL') != 'https://dhcr.clarin-dariah.eu/') {
+        echo '<font color="red"><strong>';
+        echo '**************************************************<br>';
+        echo 'WARNING:<br>';
+        echo 'You are not using the production version.<br>';
+        echo 'This data is not accurate.<br>';
+        echo 'Do not use this data!<br>';
+        echo '**************************************************<br>';
+        echo '</strong></font><br>';
+    }
+    $now = new FrozenTime('now');
+    echo '<p><u>Data updated at: ' . $now->i18nFormat('dd-MM-yyyy HH:mm') . ' UTC</u></p>';
+    ?>
+    <h3>Key data</h3>
+    <p>
+        Total: <?= $coursesTotal ?><br>
+        In backend & published: <?= $coursesBackend ?><br>
+        Public visible: <?= $coursesPublic ?><br>
+        Public as part of backend: <?= (int) ($coursesPublic / $coursesBackend * 100) ?>%
+    </p>
+    <h3>Total updated courses until months ago</h3>
+    <div id="chart_updated_courses"></div>
+    <p></p>
+    <h3>Amount of courses that will be archived soon</h3>
+    <div id="chart_archived_soon_courses"></div>
+    <p></p>
+    <p><i>Note: These statistics can change at any time. For example when a user makes changes to a course or when a certain
+            expiration period has exceeded.</i></p>
+</div>
