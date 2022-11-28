6daddd9561ad2b053821b450a6139096e6a9614b
patrickakk <patrick@algority.eu>
2022-11-28 01:15:01 +0100

    removed contact form from view

--- templates/element/info/contact.php
+++ templates/element/info/contact.php
@@ -21,53 +21,6 @@ use Cake\Core\Configure;
                 ?>
             </p>
         </div>
-        <h3>Contact Form</h3>
-        <p>
-            Please use our form to automatically let the system address
-            all moderators or admins in charge of your concern.
-        </p>
-        <?php
-        $options = [
-            'Admin Staff' => ['administrators' => 'Administrators'],
-            'National Moderators' => $countries
-        ];
-        echo $this->Form->create($email, [
-            'novalidate' => false,
-            'id' => 'ContactUsForm',
-            'class' => 'captcha-form',
-            'url' => [
-                'controller' => 'pages',
-                'action' => 'info',
-                '#' => 'contact'
-            ]
-        ]);
-        echo $this->Form->control('email', array(
-            'label' => 'Your E-Mail',
-            'autocomplete' => 'off',
-            'required' => true
-        ));
-        echo $this->Form->control('country_id', [
-            'empty' => '- choose one -',
-            'required' => true,
-            'label' => 'Send to',
-            'options' => $options
-        ]);
-        echo $this->Form->control('first_name');
-        echo $this->Form->control('last_name');
-        echo $this->Form->control('telephone', array(
-            'type' => 'text'
-        ));
-        echo $this->Form->control('message', array(
-            'type' => 'textarea',
-            'required' => true
-        ));
-        echo $this->Form->submit('Submit', array(
-            'class' => 'g-recaptcha small blue button right',
-            'data-sitekey' => Configure::read('reCaptchaPublicKey'),
-            'data-callback' => 'recaptchaCallback'
-        ));
-        echo $this->Form->end();
-        ?>
         <h3>Bug Report</h3>
         <p>
             In case you find a bug, please file a report here:
