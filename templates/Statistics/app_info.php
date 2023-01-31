e690a826d68cfba355c0c341e20314f74a07988a
patrickakk <patrick@algority.eu>
2023-01-31 10:49:01 +0100

    finished app info

--- templates/Statistics/app_info.php
+++ templates/Statistics/app_info.php
@@ -1,6 +1,18 @@
 <div class="statistics content">
     <p></p>
     <h2><span class="glyphicon glyphicon-wrench"></span>&nbsp;&nbsp;&nbsp;App Info</h2>
-    <h3>Key</h3>
-    <p>Value</p>
+    <h3>DHCR Application Version</h3>
+    <p><?php include('../VERSION.txt'); ?></p>
+    <h3>PHP Version</h3>
+    <p><?= phpversion() ?></p>
+    <h3>Debug Mode</h3>
+    <p>
+        <?php
+        if (env('debug')) {
+            echo 'DEBUG ON';
+        } else {
+            echo 'DEBUG OFF';
+        }
+        ?>
+    </p>
 </div>
