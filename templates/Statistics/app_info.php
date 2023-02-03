76567994a295f82bd7135bac519d33858c0a0868
patrickakk <patrick@algority.eu>
2023-02-03 08:00:58 +0100

    fixed displaying of debug mode

--- templates/Statistics/app_info.php
+++ templates/Statistics/app_info.php
@@ -1,23 +1,23 @@
+<?php
+
+use Cake\Core\Configure;
+
+?>
 <div class="statistics content">
     <p></p>
     <h2><span class="glyphicon glyphicon-wrench"></span>&nbsp;&nbsp;&nbsp;App Info</h2>
     <h3>DHCR Application Version</h3>
-    <p><?php
-
-        use Cake\Core\Configure;
-
-        include('../VERSION.txt'); ?></p>
+    <p><?php include('../VERSION.txt'); ?></p>
     <h3>PHP Version</h3>
     <p><?= phpversion() ?></p>
     <h3>Debug Mode</h3>
     <p>
         <?php
-        if (env('debug')) {
+        if (Configure::read('debug') == true) {
             echo 'DEBUG ON';
         } else {
             echo 'DEBUG OFF';
         }
-        debug(Configure::read('debug'));
         ?>
     </p>
 </div>
