56d272fd818bb49f1db4e25a4ed56f1c9a5a4fdd
patrickakk <patrick@algority.eu>
2023-03-25 02:03:58 +0100

    refactored routes in dhcr-main

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,9 @@
  * In this file, you set up routes to your controllers and their actions.
  * Routes are very important mechanism that allows you to freely connect
  * different URLs to chosen controllers and their actions (functions).
+ * 
+ * It's loaded within the context of `Application::routes()` method which
+ * receives a `RouteBuilder` instance `$routes` as method argument.
  *
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -19,122 +22,117 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
-use Cake\Http\Middleware\CsrfProtectionMiddleware;
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\RouteBuilder;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 
-/**
- * The default class to use for all routes
- *
- * The following route classes are supplied with CakePHP and are appropriate
- * to set as the default:
- *
- * - Route
- * - InflectedRoute
- * - DashedRoute
- *
- * If no call is made to `Router::defaultRouteClass()`, the class used is
- * `Route` (`Cake\Routing\Route\Route`)
- *
- * Note that `Route` does not do any inflections on URLs which will result in
- * inconsistently cased URLs when used with `:plugin`, `:controller` and
- * `:action` markers.
- *
- * Cache: Routes are cached to improve performance, check the RoutingMiddleware
- * constructor in your `src/Application.php` file to change this behavior.
- *
- */
-Router::defaultRouteClass(\Cake\Routing\Route\DashedRoute::class);
-
-Router::scope('/', function (RouteBuilder $routes) {
-    // Register scoped middleware for in scopes.
-    $routes->registerMiddleware('csrf', new CsrfProtectionMiddleware([
-        'httponly' => true
-    ]));
-
+return static function (RouteBuilder $routes) {
     /**
-     * Apply a middleware to the current route scope.
-     * Requires middleware to be registered via `Application::routes()` with `registerMiddleware()`
-     */
-    $routes->applyMiddleware('csrf');
-
-    /**
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, src/Template/Pages/home.ctp)...
-     */
-    $routes->connect('/', ['controller' => 'Courses', 'action' => 'index']);
-
-    $routes->connect('/iframe', ['controller' => 'Courses', 'action' => 'index']);
-    $routes->connect('/courses/iframe', ['controller' => 'Courses', 'action' => 'index']);
-    $routes->connect('/iframe/*', ['controller' => 'Courses', 'action' => 'index']);
-    $routes->connect('/courses/iframe/*', ['controller' => 'Courses', 'action' => 'index']);
-
-    $routes->connect('/info', ['controller' => 'Pages', 'action' => 'info']);
-    $routes->connect('/pages/info', ['controller' => 'Pages', 'action' => 'info']);
-
-    $routes->connect('/follow', ['controller' => 'Pages', 'action' => 'follow']);
-    $routes->connect('/national-moderators', ['controller' => 'Pages', 'action' => 'nationalModerators']);
-
-    $routes->connect('/:arg', ['controller' => 'Pages', 'action' => 'display'])->setPass(['arg'])->setPatterns(['news']);
-    //$routes->connect('/pages/news', ['controller' => 'Pages', 'action' => 'news']);
-
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-
-    // re-routing the login form irritates the Authentication Component
-    //$routes->connect('/login', ['controller' => 'Users', 'action' => 'login']);
-    //$routes->connect('/logout', ['controller' => 'Users', 'action' => 'logout']);
-    //$routes->connect('/register', ['controller' => 'Users', 'action' => 'register']);
-
-    // Added routes for the new backend
-    $routes->connect('/dashboard', ['controller' => 'Dashboard', 'action' => 'index']);
-    $routes->connect('/cities', ['controller' => 'Cities', 'action' => 'index']);
-    $routes->connect('/institutions', ['controller' => 'Institutions', 'action' => 'index']);
-    $routes->connect('/languages', ['controller' => 'Languages', 'action' => 'index']);
-    $routes->connect('/invite-translations', ['controller' => 'inviteTranslations', 'action' => 'index']);
-    $routes->connect('/logentries', ['controller' => 'Logentries', 'action' => 'index']);
-    $routes->connect(
-        '/faq-questions/{categoryId}',
-        ['controller' => 'FaqQuestions', 'action' => 'index'],
-        ["pass" => ["categoryId"], "categoryId" => "[1-3]"]
-    );
-    $routes->connect(
-        '/faq/{categoryName}',
-        ['controller' => 'FaqQuestions', 'action' => 'faqList'],
-        ["pass" => ["categoryName"]],
-    );
-    $routes->connect('/help/users-access-workflows', ['controller' => 'FaqQuestions', 'action' => 'usersAccessWorkflows']);
-    /**
-     * Connect catchall routes for all controllers.
+     * The default class to use for all routes
      *
-     * Using the argument `DashedRoute`, the `fallbacks` method is a shortcut for
+     * The following route classes are supplied with CakePHP and are appropriate
+     * to set as the default:
      *
-     * ```
-     * $routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
-     * $routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
-     * ```
-     *
-     * Any route class can be used with this method, such as:
-     * - DashedRoute
-     * - InflectedRoute
      * - Route
-     * - Or your own route class
+     * - InflectedRoute
+     * - DashedRoute
      *
-     * You can remove these routes once you've connected the
-     * routes you want in your application.
+     * If no call is made to `Router::defaultRouteClass()`, the class used is
+     * `Route` (`Cake\Routing\Route\Route`)
+     *
+     * Note that `Route` does not do any inflections on URLs which will result in
+     * inconsistently cased URLs when used with `{plugin}`, `{controller}` and
+     * `{action}` markers.
      */
-    $routes->fallbacks(\Cake\Routing\Route\DashedRoute::class);
-});
+    $routes->setRouteClass(DashedRoute::class);
 
-/**
- * If you need a different set of middleware or none at all,
- * open new scope and define routes there.
- *
- * ```
- * Router::scope('/api', function (RouteBuilder $routes) {
- *     // No $routes->applyMiddleware() here.
- *     // Connect API actions here.
- * });
- * ```
- */
+    $routes->scope('/', function (RouteBuilder $builder) {
+        // Register scoped middleware for in scopes.
+        $builder->registerMiddleware('csrf', new CsrfProtectionMiddleware([
+            'httponly' => true
+        ]));
+
+        /**
+         * Apply a middleware to the current route scope.
+         * Requires middleware to be registered via `Application::routes()` with `registerMiddleware()`
+         */
+        $builder->applyMiddleware('csrf');
+
+        /**
+         * Here, we are connecting '/' (base path) to a controller called 'Pages',
+         * its action called 'display', and we pass a param to select the view file
+         * to use (in this case, src/Template/Pages/home.ctp)...
+         */
+        $builder->connect('/', ['controller' => 'Courses', 'action' => 'index']);
+
+        $builder->connect('/iframe', ['controller' => 'Courses', 'action' => 'index']);
+        $builder->connect('/courses/iframe', ['controller' => 'Courses', 'action' => 'index']);
+        $builder->connect('/iframe/*', ['controller' => 'Courses', 'action' => 'index']);
+        $builder->connect('/courses/iframe/*', ['controller' => 'Courses', 'action' => 'index']);
+
+        $builder->connect('/info', ['controller' => 'Pages', 'action' => 'info']);
+        $builder->connect('/pages/info', ['controller' => 'Pages', 'action' => 'info']);
+
+        $builder->connect('/follow', ['controller' => 'Pages', 'action' => 'follow']);
+        $builder->connect('/national-moderators', ['controller' => 'Pages', 'action' => 'nationalModerators']);
+
+        // $builder->connect('/:arg', ['controller' => 'Pages', 'action' => 'display'])->setPass(['arg'])->setPatterns(['news']);
+        //$routes->connect('/pages/news', ['controller' => 'Pages', 'action' => 'news']);
+
+        $builder->connect('/pages/*', 'Pages::display');
+
+        // re-routing the login form irritates the Authentication Component
+        //$routes->connect('/login', ['controller' => 'Users', 'action' => 'login']);
+        //$routes->connect('/logout', ['controller' => 'Users', 'action' => 'logout']);
+        //$routes->connect('/register', ['controller' => 'Users', 'action' => 'register']);
+
+        // Added routes for the new backend
+        $builder->connect('/dashboard', ['controller' => 'Dashboard', 'action' => 'index']);
+        $builder->connect('/cities', ['controller' => 'Cities', 'action' => 'index']);
+        $builder->connect('/institutions', ['controller' => 'Institutions', 'action' => 'index']);
+        $builder->connect('/languages', ['controller' => 'Languages', 'action' => 'index']);
+        $builder->connect('/invite-translations', ['controller' => 'inviteTranslations', 'action' => 'index']);
+        $builder->connect('/logentries', ['controller' => 'Logentries', 'action' => 'index']);
+        $builder->connect(
+            '/faq-questions/{categoryId}',
+            ['controller' => 'FaqQuestions', 'action' => 'index'],
+            ["pass" => ["categoryId"], "categoryId" => "[1-3]"]
+        );
+        $builder->connect(
+            '/faq/{categoryName}',
+            ['controller' => 'FaqQuestions', 'action' => 'faqList'],
+            ["pass" => ["categoryName"]],
+        );
+        $builder->connect('/help/users-access-workflows', ['controller' => 'FaqQuestions', 'action' => 'usersAccessWorkflows']);
+        /*
+         * Connect catchall routes for all controllers.
+         *
+         * The `fallbacks` method is a shortcut for
+         *
+         * ```
+         * $builder->connect('/{controller}', ['action' => 'index']);
+         * $builder->connect('/{controller}/{action}/*', []);
+         * ```
+         *
+         * You can remove these routes once you've connected the
+         * routes you want in your application.
+         */
+        $builder->fallbacks();
+    });
+
+    /*
+     * If you need a different set of middleware or none at all,
+     * open new scope and define routes there.
+     *
+     * ```
+     * $routes->scope('/api', function (RouteBuilder $builder) {
+     *     // No $builder->applyMiddleware() here.
+     *
+     *     // Parse specified extensions from URLs
+     *     // $builder->setExtensions(['json', 'xml']);
+     *
+     *     // Connect API actions here.
+     * });
+     * ```
+     */
+};
