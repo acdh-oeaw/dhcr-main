96d0b5e5769154a618512d34eb916ffcd7ff5f35
patrickakk <patrick@algority.eu>
2023-03-30 16:37:59 +0200

    removed unnecessary css

--- templates/element/info/contact.php
+++ templates/element/info/contact.php
@@ -1,35 +1,3 @@
-<style>
-    .card {
-        -moz-border-radius: 2%;
-        -webkit-border-radius: 2%;
-        border-radius: 2%;
-        box-shadow: 5px 5px 0 rgba(0, 0, 0, 0.08);
-    }
-
-    .profile .profile-body {
-        padding: 20px;
-        background: #f7f7f7;
-    }
-
-    .profile .profile-bio {
-        background: #fff;
-        position: relative;
-        padding: 15px 10px 5px 15px;
-    }
-
-    .profile .profile-bio a {
-        left: 50%;
-        bottom: 20px;
-        margin: -62px;
-        text-align: center;
-        position: absolute;
-    }
-
-    .profile .profile-bio h2 {
-        margin-top: 0;
-        font-weight: 200;
-    }
-</style>
 <h3>Administrators</h3>
 <p>For technical questions or yet not moderated countries, please seek contact to the administration board.</p>
 <p>Before you contact us, please check the
