d8b6edae8ebdbd6a2ad5de9c482198425b4ea421
patrickakk <patrick@algority.eu>
2023-03-30 17:01:21 +0200

    added nat mod explanation

--- templates/element/info/contact.php
+++ templates/element/info/contact.php
@@ -1,5 +1,10 @@
 <h3>National Moderators</h3>
-<p>Click
+<p>
+    The course entries in the DH course registry are moderated by a group of national moderators who review and approve newly entered courses,
+    provide assistance with registration issues and disseminate the DHCR activities among the institutions of their countries.
+</p>
+<p>
+    Click
     <?= $this->Html->link('here', ['controller' => 'Pages', 'action' => 'nationalModerators']) ?>
     for an overview of all national moderators.
 </p>
@@ -8,6 +13,7 @@
 <p>If you have any other questions about the DH course registry, please check the
     <?= $this->Html->link('FAQ page', ['controller' => 'faqQuestions', 'action' => 'faqList', 'public']) ?>
     or contact us here: dhcr-helpdesk /at/ clarin-dariah.eu
+</p>
 
 <h3>Bug Report</h3>
 <p>
