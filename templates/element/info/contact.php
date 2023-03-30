7f2ff3d10170bcffe01412507cf35c68fed58be3
patrickakk <patrick@algority.eu>
2023-03-30 16:50:17 +0200

    changed text about contact admins

--- templates/element/info/contact.php
+++ templates/element/info/contact.php
@@ -4,11 +4,9 @@
     for an overview of all national moderators.
 </p>
 <h3>Administrators</h3>
-<p>For technical questions or yet not moderated countries, please seek contact to the administration board.</p>
-<p>Before you contact us, please check the
+<p>If you have any other questions about the DH course registry, please check the
     <?= $this->Html->link('FAQ page', ['controller' => 'faqQuestions', 'action' => 'faqList', 'public']) ?>
-    to see if your enquiry has already been answered.</p>
-
+    or contact us here: dhcr-helpdesk /at/ clarin-dariah.eu
 <div class="css-columns admins">
     <p>
         <?php
