d8ea1cb93b62b4083269302ac7cc68b342f2e930
ACDH-CH <105779568+acdh-ch@users.noreply.github.com>
2023-09-05 15:47:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PATH="/app/.heroku/php/bin:${PATH}"
+sed -i 's|;extension=intl|extension=intl|g' /layers/heroku_php/platform/etc/php/php.ini
+
+export PATH="/layers/heroku_php/platform/bin:${PATH}"
 
 # Generate JSON file required for searchbar
 bin/cake gen_search_list
