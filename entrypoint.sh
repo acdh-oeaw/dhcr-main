cde2383642b432613ab3554128e358143c8a8b0d
ACDH-CH <105779568+acdh-ch@users.noreply.github.com>
2023-09-06 10:39:50 +0200

    Update entrypoint.sh
    try to introduce .user.ini

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 mkdir -p /app/.heroku/php/etc/php/conf.d
-cp /layers/heroku_php/platform/etc/php/php.ini /app/.heroku/php/etc/php/conf.d/
-sed -i 's|;user_ini.filename|user_ini.filename|g' /app/.heroku/php/etc/php/conf.d/php.ini
+cp /layers/heroku_php/platform/etc/php/php.ini /app/.heroku/php/etc/php/
+sed -i 's|;user_ini.filename|user_ini.filename|g' /app/.heroku/php/etc/php/php.ini
+cp .user.ini /app/.heroku/php/etc/php/conf.d/
+
 chown -R 1000:1000 /app/.heroku
 
 export PATH="/layers/heroku_php/platform/bin:${PATH}"
