726bf5640cc000e6c4ab164c001947fb53e4b3ce
patrickakk <patrick@algority.eu>
2023-12-12 14:59:26 +0100

    add logos #87 partitially

--- templates/layout/home.php
+++ templates/layout/home.php
@@ -15,7 +15,7 @@ use Cake\Core\Configure;
 <body class="dhcr-home <?= $this->request->getParam('action') ?>">
     <div id="page-head">
         <div id="logo">
-            <?= $this->Html->image('logo-300.png', [
+            <?= $this->Html->image('header_logo.png', [
                 'url' => '/',
                 'alt' => 'Digital Humanities Course Registry (logo)'
             ]); ?>
