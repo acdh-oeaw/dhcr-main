944469654ff1a6afafc0764be6e202001d01fdbf
patrickakk <patrick@algority.eu>
2024-01-30 19:34:36 +0100

    disabled sibboleth discovery in entrypoint since unused

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ bin/cake gen_search_list
 # Generate XML sitemap
 bin/cake gen_sitemap
 
-# Fetch data for Shibboleth login
-bin/cake discovery
+## Fetch data for Shibboleth login
+#bin/cake discovery
 
 # Start web server
 exec heroku-php-apache2
