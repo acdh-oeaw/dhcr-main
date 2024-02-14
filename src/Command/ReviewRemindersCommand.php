e92bc208c4ce9027fca77966efb86ad163e1130e
patrickakk <patrick@algority.eu>
2024-02-14 16:58:39 +0100

    added review reminders

---
+++ src/Command/ReviewRemindersCommand.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Command;
+
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Mailer\Mailer;
+use Exception;
+
+class ReviewRemindersCommand extends Command
+{
+    private function getUseradmins()
+    {
+        $useradmins = $this->Users->find()->where([
+            'user_admin' => 1,
+            'active' => 1,
+        ]);
+        return $useradmins;
+    }
+
+    private function generateOneliner() {
+        $oneliners = [
+            "Reviewing GitHub issues is like being the hero in a digital adventure – no cape required, just a willingness to make the code world a better place!",
+            "Think of GitHub issues as your code's post-it notes – reviewing them is like giving your project a daily dose of sticky motivation!",
+            "You don't need to speak binary to be a GitHub superhero – reviewing issues is the non-technical way to save the day for your favorite project!",
+            "GitHub issues are like the breadcrumbs in the coding forest – follow them, and you'll end up making the codebase a magical place for everyone!",
+            "Reviewing GitHub issues is like being the detective in a code mystery novel – no Sherlock hat needed, just a curious mind and a cup of coffee!",
+            "You don't have to be a coding wizard to review GitHub issues – just think of it as giving your project a digital high-five for a job well done!",
+            "GitHub issues are the jigsaw puzzles of the coding world – no need for a computer science degree, just a knack for putting pieces together!",
+            "Reviewing GitHub issues is the non-technical equivalent of decluttering your code's closet – because even projects need a Marie Kondo moment!",
+            "You don't need a PhD in programming to be a GitHub guru – reviewing issues is the passport to the exciting world of code improvement!",
+            "GitHub issues are like the IKEA instructions of coding – even non-techies can help assemble a better project with just a bit of review magic!",
+            "Reviewing GitHub issues is the code version of being a friendly neighbor – you don't need to understand the language, just be willing to lend a helping hand!",
+            "You don't have to speak in code to contribute – reviewing GitHub issues is like leaving positive comments on your project's digital fridge!",
+            "GitHub issues are the paint-by-numbers of the coding canvas – even if you can't draw a straight line of code, you can help add some color to the project!",
+            "Reviewing GitHub issues is like being the project's motivational speaker – no technical jargon needed, just a few words of encouragement for your code comrades!",
+            "You don't need a PhD in algorithms to make a difference – reviewing GitHub issues is the non-geeky way to sprinkle some joy into your project's code!"
+        ];
+        $randomIndex = array_rand($oneliners);
+        return $oneliners[$randomIndex];
+    }
+
+    private function sendReviewReminders($issuesAmount, $issuesUrl)
+    {
+        $useradmins = $this->getUseradmins();
+        $subject = "[DH Course Registry] - Your Attention Needed on $issuesAmount GitHub Issues";
+        $oneliner = $this->generateOneliner();
+        $totalMails = 0;
+        foreach ($useradmins as $useradmin) {
+            try {
+                $mailer = new Mailer();
+                $mailer->setFrom(env('APP_MAIL_DEFAULT_FROM'))
+                    ->setSender(env('APP_MAIL_DEFAULT_FROM'))
+                    ->setReturnPath(env('APP_MAIL_DEFAULT_FROM'))
+                    ->setTo($useradmin->email)
+                    ->setSubject($subject)
+                    ->setViewVars(['firstName' => $useradmin->first_name,
+                                    'issuesAmount' => $issuesAmount,
+                                    'issuesUrl' => $issuesUrl,
+                                    'oneliner' => $oneliner])
+                    ->viewBuilder()->setTemplate('review_reminders/review_reminder');
+                $mailer->deliver();
+                $totalMails++;
+            } catch (Exception $ex) {
+                $action = 'Error sending mail';
+                $details = 'Useradmin: ' .$useradmin->email;
+                echo "$action $details \n";
+                $scriptName = basename(__FILE__, '.php');
+                $scriptName = str_replace('Command', '', $scriptName);
+                $this->Logentries->createLogEntry(
+                    '50',
+                    '586',
+                    $scriptName,
+                    $action,
+                    $details
+                );
+            }
+            echo ".";   // progress indicator
+        }
+        echo "\n";      // close progress indicator        
+        $action = 'Result:';
+        $details = $totalMails . ' mails sent.';
+        $scriptName = basename(__FILE__, '.php');
+        $scriptName = str_replace('Command', '', $scriptName);
+        $this->Logentries->createLogEntry(
+            '30',
+            '586',
+            $scriptName,
+            $action,
+            $details
+        );
+    }
+
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $this->loadModel('Users');
+        $this->loadModel('Logentries');
+        $io->out('~~~ Started Review Reminders ~~~');
+        $issuesUrl = 'https://github.com/acdh-oeaw/dhcr-main/labels/InReview';
+        $content = file_get_contents($issuesUrl);
+        $pattern = '/\b\d+\sOpen\b/i';
+        if (preg_match($pattern, $content, $lines)) {
+            preg_match('/\d+/', $lines[0], $result);
+            $issuesAmount = $result[0];
+            $io->out('Amount of open issues: ' . $issuesAmount);
+            if ($issuesAmount > 0) {
+                $this->sendReviewReminders($issuesAmount, $issuesUrl);
+            } else {
+                $io->out('No issues open');
+                $this->Logentries->createLogEntry(
+                    '30',
+                    '586',
+                    basename(__FILE__, '.php'),
+                    'Result:',
+                    'No issues open.'
+                );
+            }
+        } else {
+            $action = 'Error:';
+            $details = 'Error finding amount of open issues';
+            $io->out($details);
+
+            $scriptName = basename(__FILE__, '.php');
+            $scriptName = str_replace('Command', '', $scriptName);
+            $this->Logentries->createLogEntry(
+                '50',
+                '586',
+                $scriptName,
+                $action,
+                $details
+            );
+        }
+        $io->out('~~~ Finished Review Reminders ~~~');
+    }
+}
---
+++ templates/email/text/review_reminders/review_reminder.php
@@ -0,0 +1,49 @@
+Dear <?=$firstName?>,
+
+I hope this message finds you well. We want to bring to your attention the 
+importance of reviewing and addressing GitHub issues that have been raised. 
+These issues are crucial to the progress of the project, and your input is 
+invaluable in ensuring a smooth and efficient development process.
+
+There are currently a total of <?=$issuesAmount?> issue(s) waiting for your review.
+
+GitHub issues serve as a centralized platform for identifying, discussing, and 
+resolving tasks, bugs, and feature requests. Your expertise is needed to review 
+these items promptly, as it directly impacts the overall success of the project. 
+Here are a few reasons why your timely attention is crucial:
+
+1. Efficient Problem Resolution: Your insights and expertise are vital in identifying 
+potential issues, clarifying requirements, and proposing effective solutions. By 
+reviewing and responding promptly, we can address problems before they escalate and 
+contribute to a more streamlined development process.
+
+2. Collaborative Decision-Making: GitHub issues foster collaboration and provide a space 
+for team members to share thoughts, ask questions, and make informed decisions collectively. 
+Your input plays a significant role in shaping the direction of our projects and ensuring 
+that everyone is on the same page.
+
+3. Project Momentum: Timely reviews and resolutions help maintain project momentum. Delays 
+in addressing issues can hinder progress, impact deadlines, and potentially lead to a 
+less-than-optimal end product. Your active participation is crucial to keeping the development 
+process moving forward smoothly.
+
+To assist you in navigating GitHub issues, we have included the following link:
+<?=$issuesUrl?>
+
+
+Your prompt attention to GitHub issues is highly appreciated and will contribute 
+significantly to the success of the DHCR project. Thank you for your dedication
+and collaboration.
+
+*Saying of the week*
+<?=$oneliner?>
+
+
+Note:
+Please don't reply to this email, since it's sent as part of an automated process which 
+notifies you weekly when GitHub issues are waiting for your review.
+
+
+Best regards,
+
+Your DHCR Developer
