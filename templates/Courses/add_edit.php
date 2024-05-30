b81300724041465b0c553da791bf6fd84964bca9
patrickakk <patrick@algority.eu>
2024-05-30 13:36:25 +0200

    add training material ui preview #48

--- templates/Courses/add_edit.php
+++ templates/Courses/add_edit.php
@@ -23,6 +23,8 @@
                 echo $this->Form->control('contact_mail', ['label' => 'Lecturer E-Mail']);
                 echo $this->Form->control('description', ['label' => 'Description *', 'placeholder' => 'Please add in English the general aims of the course/programme and the learning outcomes.']);
                 echo $this->Form->control('access_requirements', ['label' => 'Entry Requirements', 'placeholder' => 'For instance: if you want to enroll in this MA module, you need to hold a BA degree in X, Y, Z']);
+                echo $this->Form->control('training_url', ['label' => 'Training Material URL', 'placeholder' => 'Optional: The public web address of related training material']);
+                echo '<p>Make sure to enter a valid URL, preferbly use https:// alternatively use http://';
                 echo '<p id="start_date">&nbsp;</p>';   // internal url
                 echo $this->Form->control('start_date', ['label' => 'Start Date*', 'onclick' => 'openForm()', 'placeholder' => 'YYYY-MM-DD']);
                 ?>
