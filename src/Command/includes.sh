e16d3792025b66a7b35509e941ee1885b3bf9f67
patrickakk <patrick@algority.eu>
2024-10-05 18:19:00 +0200

    Revert "added cd to includes.sh"
    This reverts commit f7947648cf71d89767f90a8078c598efb8ded755.

--- src/Command/includes.sh
+++ src/Command/includes.sh
@@ -4,7 +4,6 @@ function prepare_php() {
     # set path
     export PATH="/layers/heroku_php/platform/bin:${PATH}"
     # enable intl module
-    cd / # avoid error when running this file from its folder
     mkdir -p /app/.heroku/php/etc/php/conf.d
     cp /layers/heroku_php/platform/etc/php/php.ini /app/.heroku/php/etc/php/
     sed -i 's|;user_ini.filename|user_ini.filename|g' /app/.heroku/php/etc/php/php.ini
