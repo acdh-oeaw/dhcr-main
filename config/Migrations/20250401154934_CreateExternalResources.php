00ca1473ff8ba9c8ba75870fa9291f682caca3d1
patrickakk <patrick@algority.eu>
2025-04-01 18:10:06 +0200

    created migration for external_resources #48

---
+++ config/Migrations/20250401154934_CreateExternalResources.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateExternalResources extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('external_resources');
+
+        $table->addColumn('label', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('url', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('type', 'string', [
+            'default' => null,
+            'limit' => 100,
+            'null' => true,
+        ]);
+        $table->addColumn('affiliation', 'string', [
+            'default' => null,
+            'limit' => 100,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('updated', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
